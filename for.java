dd1d6aa85956e5b71aa5167b666dab5f313de56a
Guna  K K <kkguna58@gmail.com>
2026-07-17 17:36:28 +0530

    Correct for loop and print logic in for.java
    Fix syntax errors in the for loop and print statement.

---
+++ for.java
@@ -0,0 +1,14 @@
+public class for {
+    public static void main(String[] args) {
+        for (int i = 2005); {
+          
+            if(i%4==0){
+                System.out.prinln("leap")
+            }else{
+                System.out.println(i);
+
+            }
+        }
+    }
+    
+}
