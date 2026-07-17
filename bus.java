ddeb90ecacf9ad1ebd9b0a6d7912bfde00fd6e64
Guna  K K <kkguna58@gmail.com>
2026-07-17 17:37:35 +0530

    Add Main class to print numbers and 'buss' at multiples of 5

---
+++ bus.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=50){
+            if(i%5==0){
+                System.out.println("buss");
+                i++;
+            }else{
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
