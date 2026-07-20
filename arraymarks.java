cfcdfed13412fd649a6a17034ba9f65f9f02a7b8
Guna  K K <kkguna58@gmail.com>
2026-07-20 14:59:04 +0530

    Add arraymarks.java to display subject marks

---
+++ arraymarks.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       int [] marks={50,60,70,80,90};
+       String []subject={"tamil","english","maths","science","social"};
+       for(int i=0;i<marks.length;i++)
+       {
+           System.out.println(subject[i]+"="+marks[i]);
+       }
+    }
+}
+
+
