6de8bf8d2776facb9780c5d1a655fe1c47660e8b
Guna  K K <kkguna58@gmail.com>
2026-07-22 17:53:29 +0530

    Add uppercaselowercase.java to demonstrate string case

---
+++ uppercaselowercase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class uppercaselowercase {
+    public static void main(String[] args) {
+        String name = "java";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
