0b15eb9432522f8314e8b8d975479aba02aba26f
Guna  K K <kkguna58@gmail.com>
2026-07-22 18:01:57 +0530

    Add length.java to print string length

---
+++ length.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class length {
+    public static void main(String[] args) {
+        String name = "programming";
+        System.out.println(name.length());
+    }
+}
