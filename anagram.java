47ad5b06b4a176bf88d083b7e9d9db5251c25890
Guna  K K <kkguna58@gmail.com>
2026-07-28 16:08:01 +0530

    Add anagram checking functionality in anagram.java
    Implement a basic anagram checker that compares two strings.

---
+++ anagram.java
@@ -0,0 +1,22 @@
+public class anagram{
+    public static void main(String[]args){
+        String str="silent";
+        String str1="listen";
+        char[]arr=str.toCharArray();
+        char[]arr1=str1.toCharArray();
+        if(arr.length!=arr1.length)
+        {
+            System.out.println("Not an anagram");
+            return;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=arr[i])
+            {
+                System.out.println("Not an anagram");
+                return;
+            }
+    }
+    System.out.println("An anagram");
+}
+}
